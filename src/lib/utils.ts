6559fd8e9ba281bda0415a530c5375dc9bba979f
fredle09 <le197903@gmail.com>
2025-08-24 01:20:57 +0700

    [refactor]: update next.config.ts structure and remove unused rootDomain variable from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,9 +3,6 @@ import { twMerge } from "tailwind-merge";
 
 import type { ClassValue } from "clsx";
 
-export const rootDomain =
-  process.env.NEXT_PUBLIC_ROOT_DOMAIN || "localhost:3000";
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
